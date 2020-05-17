effef3c1ce80e6e4c3ce9abcd7ff86ecca4c918a
Carlos David <ing.carlosgarciabendahan@gmail.com>
2020-05-17 12:32:24 -0400

    config-client CREATED

---
+++ config-client/src/main/java/cl/carlos/config/client/config/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package cl.carlos.config.client.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
---
+++ config-client/src/test/java/cl/carlos/config/client/config/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.carlos.config.client.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
