b4abe33b006f456cdd701cc9489458354e7a23ee
Carlos David <ing.carlosgarciabendahan@gmail.com>
2020-05-17 12:13:32 -0400

    v1.0.0 config-server

---
+++ config-server/src/test/java/cl/carlos/config/server/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.carlos.config.server.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
